6b4f7a862832e6027dde5b51529b4f146bbecb10
Cno <dongheming@bytedance.com>
2024-01-15 19:53:56 +0800

    feat: use sw to match pwa

--- src/sw.ts
+++ src/sw.ts
@@ -3,6 +3,21 @@ import { StaleWhileRevalidate } from 'workbox-strategies';
 
 console.log((self as any).__WB_MANIFEST);
 registerRoute(
-  ({ request }) => request.method === 'GET',
-  new StaleWhileRevalidate(),
+  ({ request }) => {
+    const types: RequestDestination[] = [
+      'style',
+      'document',
+      'script',
+      'image',
+      '',
+    ];
+    // console.log(114514, request.url, request.destination);
+    return types.includes(request.destination);
+  },
+  new StaleWhileRevalidate({
+    matchOptions: {
+      ignoreVary: true,
+      ignoreSearch: true,
+    },
+  }),
 );
